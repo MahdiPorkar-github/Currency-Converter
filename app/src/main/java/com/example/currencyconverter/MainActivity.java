abc85f3e26f0bfa766631e72f1f4c0e6d6ac1b42
Mahdi <MahdiPorkar2002@gmail.com>
2021-12-01 23:03:04 +0330

    Created the app

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void pressConvertButton(View view) {
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+        String amount = amountEditText.getText().toString();
+        String convertedAmount = (Integer.parseInt(amount) * 0.88) + "";
+        Toast.makeText(this, amount + "$ = " + convertedAmount + "€", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
